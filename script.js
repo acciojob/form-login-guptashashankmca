0d8a84f403c026cd8ff921a9213ec6797c489fdf
Shashank Gupta <165798455+guptashashankmca@users.noreply.github.com>
2025-05-23 12:45:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
-// function getFormvalue(e){
-// 	e.preventDefault();
-// 	const firstName = document.querySelector('input[name="fname"]').value.trim();
-// 	const secondName = document.querySelector('input[name="lname"]').value.trim();
-// 	alert(firstName+" "+secondName);
-// }
-  
-    function getFormvalue(e) {
+function getFormvalue(e) {
       e.preventDefault(); // Prevent page reload
 
       const firstName = document.querySelector('input[name="fname"]').value.trim();
